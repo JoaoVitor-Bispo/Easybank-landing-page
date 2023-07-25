966eda5ca0584b024b40867e11a1680dce96e1eb
gabrieljuniodasilvabispo <joaovitor.bispo080@gmail.com>
2023-07-25 14:13:38 -0300

    final

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
 const menu = document.querySelector('#menu');
-const otherLinks = [...document.getElementsByClassName('header-links')];
+const links = [...document.querySelectorAll('.header-links')]
 
-const newDiv = document.createElement('div');
 let menuImage = false
+const newDiv = document.createElement('div');
 newDiv.id = 'linksHidden'
 
 menu.addEventListener('click', (e) => {
-    menuImage === false ? menuImage = true : menuImage = false
-    
-    if(menuImage === true) {
-        menu.src = '/images/icon-close.svg'
-
-        otherLinks.map((element) => {
-            newDiv.appendChild(element)
-        })
-    
-        document.documentElement.append(newDiv)
-        document.querySelector('.images').style.opacity = '0.4'    
-    }
-    else {
-        menu.src = '/images/icon-hamburger.svg'
-        document.querySelector('.images').style.opacity = '1'    
-        newDiv.remove()
+    if(document.documentElement.offsetWidth <= '900') {
+        [...newDiv.childNodes].map(element => element.remove())
+        menuImage === false ? menuImage = true : menuImage = false
+        
+        if(menuImage === true) {
+            menu.src = '/images/icon-close.svg'
+            
+            links.map((element) => {
+                const newLinks = document.createElement('a')
+                newLinks.textContent = element.textContent
+                newLinks.href = element.href
+                newDiv.appendChild(newLinks)
+            })
+            
+            document.documentElement.append(newDiv)
+            document.querySelector('.images').style.opacity = '0.4'    
+        }
+        else {
+            menu.src = '/images/icon-hamburger.svg'
+            document.querySelector('.images').style.opacity = '1'    
+            newDiv.remove()
+        }
     }
 })
