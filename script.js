3eb8e8424a753123e536b32475245a4164cebad7
gabrieljuniodasilvabispo <joaovitor.bispo080@gmail.com>
2023-08-23 16:01:22 -0300

    desbug menu

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
 const menu = document.querySelector('#menu');
-const links = [...document.querySelectorAll('.header-links')]
+const links = [...document.querySelectorAll('.header-links')];
 
-let menuImage = false
+let menuImage = false;
 const newDiv = document.createElement('div');
-newDiv.id = 'linksHidden'
+newDiv.id = 'linksHidden';
 
 menu.addEventListener('click', (e) => {
-    menuImage === false ? menuImage = true : menuImage = false
+    menuImage === false ? menuImage = true : menuImage = false;
     
     if(menuImage === true) {
-        menu.src = './images/icon-close.svg'
+        menu.src = './images/icon-close.svg';
         
         links.map((element) => {
-            const newLinks = document.createElement('a')
-            newLinks.textContent = element.textContent
-            newLinks.href = element.href
-            newDiv.appendChild(newLinks)
-        })
+            const newLinks = document.createElement('a');
+            newLinks.textContent = element.textContent;
+            newLinks.href = element.href;
+            newDiv.appendChild(newLinks);
+        });
         
-        document.documentElement.append(newDiv)
-        document.querySelector('.images').style.opacity = '0.4'
-        return 0
-    }
-    menu.src = '/images/icon-hamburger.svg'
-    document.querySelector('.images').style.opacity = '1'    
-    newDiv.remove()
+        document.documentElement.append(newDiv);
+        document.querySelector('.images').style.opacity = '0.4';
+        return false;
+    };
+    
+    menu.src = '/images/icon-hamburger.svg';
+    document.querySelector('.images').style.opacity = '1';
+
+    [...newDiv.children].map(element => element.remove())
+    newDiv.remove();
 })
